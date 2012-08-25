ccdc7462e7ca6ff599c6000fb7d3bd12dbf7cc42
Rustam Zagirov <rustam@zagirov.name>
2012-08-25 10:52:10 +0400

    rails generate scaffold Post title:string content:text content_display:text status:tinyint post_time:integer author_id:integer url:string short_url:string

---
+++ db/migrate/20120825065041_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.text :content_display
+      t.tinyint :status
+      t.integer :post_time
+      t.integer :author_id
+      t.string :url
+      t.string :short_url
+
+      t.timestamps
+    end
+  end
+end
