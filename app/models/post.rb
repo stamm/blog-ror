c9a84543aac841c8aa2917b37c296175a0b5c56f
Rustam Zagirov <rustam@zagirov.name>
2012-08-25 17:28:16 +0400

    add first validation in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :author_id, :content, :content_display, :post_time, :short_url, :status, :title, :url
+
+  validates :title, :content, :post_time, :url, :status, presence: true
 end
