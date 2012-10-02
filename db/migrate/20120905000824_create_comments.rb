809895eb47e39de2704be1dd1f845836243cb919
Rustam Zagirov <rustam@zagirov.name>
2012-10-02 20:02:13 +0400

    fix travis

--- db/migrate/20120905000824_create_comments.rb
+++ db/migrate/20120905000824_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :content, null: false, default: ''
+      t.text :content, null: false
       t.integer :status, null: false, default: 0
       t.string :author, null: false, default: ''
       t.string :email, null: false, default: ''
