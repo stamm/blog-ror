8e0944ad7edd404aba7b47831a5af1f911d2f532
Rustam Zagirov <rustam@zagirov.name>
2012-10-05 19:41:35 +0400

    separate production & staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 require "rvm/capistrano"
 require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
 require 'capistrano_colors'
 
 default_run_options[:pty] = true
@@ -7,10 +8,17 @@ default_run_options[:pty] = true
 set :application, "blog-ror"
 set :repository,  "git://github.com/Stamm/blog-ror.git"
 
+set :stages,        %w(staging production)
+set :default_stage, "staging"
+set :branch,        "master"
+
 
 set :rails_env, "production"
 set :using_rvm, true
 set :rvm_type, :user
+set :rvm_ruby_string, 'ruby-1.9.3-p194@gemset'
+# интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
+before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
 
 
 server "174.129.240.218", :web, :app
@@ -20,10 +28,10 @@ set :use_sudo, false
 
 set :scm, :git
 set :scm_verbose, true
-set :deploy_via, :remote_cache
+#set :deploy_via, :remote_cache
 set :branch, "master"
 set :keep_releases, 4
-set :deploy_to, "/var/www/#{application}"
+#set :deploy_to, "/var/www/#{application}"
 
 
 after "deploy:update", "deploy:cleanup"
@@ -33,33 +41,79 @@ set :bundle_without, [:test, :development]
 set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
 set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
-# интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
-before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
 
 ####################
 
 ##set :rvm_ruby_string, 'ree' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
 #set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
-set :rvm_ruby_string, 'ruby-1.9.3-p194@gemset'
 
 
-after 'deploy:update_code', :roles => :app do
-  # Здесь для примера вставлен только один конфиг с приватными данными - database.yml.
-  # Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда.
-  # При каждом деплое создаются ссылки на них в нужные места приложения.
-  run "rm -f #{current_release}/config/database.yml"
-  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
-end
+
+
+# Optional
+before "deploy",                 "deploy:web:disable"
+before "deploy:stop",            "deploy:web:disable"
+
+after  "deploy:update_code",     "deploy:symlink_shared"
+
+# Optional
+after  "deploy:start",           "deploy:web:enable"
+after  "deploy",                 "deploy:web:enable"
+
+after  "deploy",                 "deploy:cleanup"
+
+
 
 # Далее идут правила для перезапуска unicorn. Их стоит просто принять на веру - они работают.
 namespace :deploy do
+  desc "restart unicorn server"
   task :restart do
     run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
   end
+
+  desc "start unicorn server"
   task :start do
-    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+    run "cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
   end
+
+  desc "stop unicorn server"
   task :stop do
     run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+  end
+
+
+  desc "Link in the production database.yml and assets"
+  task :symlink_shared do
+    # Здесь для примера вставлен только один конфиг с приватными данными - database.yml.
+    # Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда.
+    # При каждом деплое создаются ссылки на них в нужные места приложения.
+    run "rm -f #{current_release}/config/database.yml"
+    run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
+  end
+
+  namespace :web do
+
+    desc "Maintenance start"
+    task :disable, :roles => :web do
+      on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+      page = File.read("public/503.html")
+      put page, "#{shared_path}/system/maintenance.html", :mode => 0644
+    end
+
+    desc "Maintenance stop"
+    task :enable, :roles => :web do
+      run "rm #{shared_path}/system/maintenance.html"
+    end
+  end
+end
+
+
+namespace :log do
+  desc "A pinch of tail"
+  task :tailf, :roles => :app do
+    run "tail -n 10000 -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
+      puts "#{data}"
+      break if stream == :err
+    end
   end
 end
