db33f52570e73d0a0b081909475ec8797d440e01
Rustam Zagirov <rustam@zagirov.name>
2013-02-17 17:01:46 +0400

    fix migration to postgresql

--- db/migrate/20120902215821_create_user_stamm.rb
+++ db/migrate/20120902215821_create_user_stamm.rb
@@ -1,6 +1,6 @@
 class CreateUserStamm < ActiveRecord::Migration
   def up
-    execute "INSERT INTO `users` (`name`, `password_digest`, `created_at`, `updated_at`)
+    execute "INSERT INTO users (name, password_digest, created_at, updated_at)
 VALUES
 	('stamm', '$2a$10$1r.etpQZI/dhOoMVfogYNOoq3IM3l9B0qvXf7n16PJwrL26OkyjK2', '2012-09-02 21:56:36', '2012-09-02 21:56:36');"
   end
