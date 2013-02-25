b0f75aa0348b3860468a5b3751f04ad4d8fe6c5f
Rustam Zagirov <rustam@zagirov.name>
2013-02-25 15:30:25 +0400

    change password for my user

--- db/migrate/20120902215821_create_user_stamm.rb
+++ db/migrate/20120902215821_create_user_stamm.rb
@@ -2,7 +2,7 @@ class CreateUserStamm < ActiveRecord::Migration
   def up
     execute "INSERT INTO users (name, password_digest, created_at, updated_at)
 VALUES
-	('stamm', '$2a$10$1r.etpQZI/dhOoMVfogYNOoq3IM3l9B0qvXf7n16PJwrL26OkyjK2', '2012-09-02 21:56:36', '2012-09-02 21:56:36');"
+	('stamm', '$2a$10$h8XuUFniLlYoiRT2VuPpLOld5xXl0sP95rSWNVPSi7ewQM1QUcXCy', '2012-09-02 21:56:36', '2012-09-02 21:56:36');"
   end
 
   def down
