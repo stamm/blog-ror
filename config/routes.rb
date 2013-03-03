e76b6eba81d1f093132c3dcff0c74ca34f985e24
Rustam Zagirov <rustam@zagirov.name>
2013-03-03 04:23:41 +0400

    fix routes: disabled formats

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,33 @@
 ZagirovName::Application.routes.draw do
-  #mount Markitup::Rails::Engine, at: "markitup", as: "markitup"
+  #mount Markitup::Rails::Engine, at: 'markitup', as: 'markitup'
 
-  get "/about" => "static_pages#about"
-  post "/markitup/preview" => "markitup#preview"
 
-  controller :sessions do
-    get 'login' => :new, as: 'login'
-    post 'login' => :create
-    delete 'logout' => :destroy, as: 'logout'
-  end
+  constraints :format => // do
+    root to: 'main#posts'
+    get '/about' => 'static_pages#about'
+    post '/markitup/preview' => 'markitup#preview'
 
-  root to: 'main#posts'
-  #get "/" => "main#posts"
-  get "/posts/page/:page" => "main#posts"
 
-  controller :main do
-    get "/tags" => :tags
-  end
-  get "/tag/:tag" => "main#posts", page: '1'
-  get "/tag/:tag/:page" => "main#posts"
+    controller :sessions do
+      get 'login' => :new
+      post 'login' => :create
+      delete 'logout' => :destroy
+    end
 
-  namespace :admin do
-    get '' => 'admin#index'
-    resources :comments, :users
-    resources :posts do
-      get 'page/:page', :action => :index, :on => :collection
+    namespace :admin do
+      get '' => 'admin#index'
+      resources :comments, :users
+      resources :posts do
+        get 'page/:page', :action => :index, :on => :collection
+      end
+    end
+
+    controller :main do
+      get '/posts/page/:page' => :posts
+      get '/tags' => :tags
+      get '/tag/:tag' => :posts, page: '1'
+      get '/tag/:tag/:page' => :posts
+      match '/:url' => :article, as: :article, via: [:get, :post]
     end
   end
-
-  match "/:url" => "main#article", as: :article, via: [:get, :post]
-
 end
