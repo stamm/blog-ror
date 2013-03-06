836f877bb2c2449a741a0ac43dabc54984af1561
Rustam Zagirov <rustam@zagirov.name>
2013-03-06 04:54:32 +0400

    fix configs

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,25 @@ module ZagirovName
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0.2'
+
+    config.assets.precompile += %w(main.js main.css sessions.js sessions.css)
+    config.assets.precompile += %w(admin.js admin.css)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: true
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
