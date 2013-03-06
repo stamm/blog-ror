5fdb010a6af2845d38d946d474c1f6b865c9e787
Rustam Zagirov <rustam@zagirov.name>
2013-03-06 04:58:49 +0400

    update assets version

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module ZagirovName
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0.2'
+    config.assets.version = '1.0.3'
 
     config.assets.precompile += %w(main.js main.css sessions.js sessions.css)
     config.assets.precompile += %w(admin.js admin.css)
