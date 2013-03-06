77cc595be5222c54f911599a22223daec06985a4
Rustam Zagirov <rustam@zagirov.name>
2013-03-06 06:34:30 +0400

    restart unicorn after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,6 +60,7 @@ after  "deploy:start",           "deploy:web:enable"
 after  "deploy",                 "deploy:web:enable"
 
 after  "deploy",                 "deploy:cleanup"
+after  "deploy",                 "deploy:restart"
 
 
 
@@ -100,16 +101,7 @@ namespace :deploy do
       CMD
     end
   end
-  #namespace :assets do
-  #  task :precompile, :roles => :web, :except => { :no_release => true } do
-  #    #from = source.next_revision(current_revision)
-  #    #if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-  #      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} assets:precompile}
-  #    #else
-  #    #  logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #    #end
-  #  end
-  #end
+
 
   namespace :web do
 
