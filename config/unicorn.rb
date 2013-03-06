fc9cf3ef2c94a2620dff2c58302455810281e61a
Rustam Zagirov <rustam@zagirov.name>
2013-03-06 04:16:38 +0400

    fix unicorn path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-deploy_to  = "#{__dir__}/../.."
+deploy_to  = "#{File.expand_path(File.dirname(File.dirname(__FILE__)))}/../.."
 rails_root = "#{deploy_to}/current"
 pid_file   = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
