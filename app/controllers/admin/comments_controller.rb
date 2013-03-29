e921bc30df79a6e6d66c8a11407e1834f2c3a9fa
Rustam Zagirov <rustam@zagirov.name>
2013-03-29 19:02:23 +0400

    comments admin

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,5 +1,50 @@
 class Admin::CommentsController < ApplicationController
-  skip_before_filter :authorize
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  def index
+    @comments = Comment.order(created_at: :desc).page(params[:page]).per(40)
+  end
+
+  def show
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def edit
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to admin_comment_url(@comment), notice: 'Comment was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @comment.update_attributes(comment_params)
+      redirect_to admin_comment_url(@comment), notice: 'Comment was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    redirect_to admin_comments_url
+  end
 
 
+  private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :author, :email, :url)
+  end
 end
