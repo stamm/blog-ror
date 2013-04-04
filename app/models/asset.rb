876e58055738384d3608716810d58febf6ed0808
Rustam Zagirov <rustam@zagirov.name>
2013-04-04 18:28:29 +0400

    asset keep only thumb

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -2,9 +2,7 @@ class Asset < ActiveRecord::Base
   belongs_to :post
   has_attached_file :image,
     styles: {
-        thumb: '100x100#',
-        small: '300x300>',
-        large: '600x600>'
+        thumb: '600x600>',
     }
   validates_attachment_content_type :image, content_type: /image/
   validates_attachment_size :image, in: 0..10.megabytes
